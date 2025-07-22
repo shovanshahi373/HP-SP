238631d9e5e1a4c5f8e7947bc11707aadb0a4f55
shovan shahi <shovanshahi0373@gmail.com>
2025-07-23 00:04:29 +0545

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import path from "path";
 import dotenv from "dotenv";
+
 const env = process.env.NODE_ENV || "development";
-dotenv.config({ path: `.env.${env}` });
+
+if (env !== "production") {
+  dotenv.config({ path: `.env.${env}` });
+}
 
 import puppeteer, { Page } from "puppeteer";
 
