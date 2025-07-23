d5c7bc8e94773820522c19648cbf3632f84cba1a
shovan shahi <shovanshahi0373@gmail.com>
2025-07-23 12:03:30 +0545

    Docker

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM ghcr.io/puppeteer/puppeteer:24.14.0
+
+ENV PUPPETEER_SKIP_CHROMUIM_DOWNLOAD=true \
+    PUPPETEER_EXECUTABLE_PATH=/usr/bin/google-chrome-stable
+
+WORKDIR /usr/src/app
+
+COPY package*.json ./
+RUN npm ci
+COPY . .
+RUN npx tsc
+CMD [ "node", "build/index.js" ]
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-import path from "path";
 import dotenv from "dotenv";
 
 const env = process.env.NODE_ENV || "development";
@@ -7,121 +6,18 @@ if (env !== "production") {
   dotenv.config({ path: `.env.${env}` });
 }
 
-import puppeteer, { Page } from "puppeteer";
+import express from "express";
 
-import { loadSecrets } from "./config/secretManager";
+import "./jobs";
 
-import { create } from "./services/notification";
+const PORT = process.env.PORT || 4000;
 
-import { ICON_URL } from "./constants";
-(async () => {
-  await loadSecrets();
+const app = express();
 
-  const { sendAlert } = create(globalThis.SECRETS.HYPE_PACKS_MINER);
+app.get("/", (req, res) => {
+  res.send("refreshed");
+});
 
-  const filterKeyword = "====>";
-
-  const visitURL = async (page: Page, topic: string) => {
-    await page.setBypassCSP(true);
-    await page.goto(
-      "https://discord.com/channels/1362205838550237224/1372478920204746802"
-    );
-    try {
-      await page.addScriptTag({
-        path: path.resolve(__dirname, "../dist/bundle.js"),
-      });
-      await page.evaluate(
-        (filterKeyword, topic, version) =>
-          window["MyBundle"].rejoin(filterKeyword, topic, version, false),
-        filterKeyword,
-        topic,
-        globalThis.SECRETS.VERSION
-      );
-
-      page.on("console", (msg) => {
-        const text = msg.text();
-        if (text.includes(filterKeyword)) {
-          return console.log("PAGE LOG>", text);
-        }
-      });
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const login = async (
-    page: Page,
-    credentials: { email: string; password: string }
-  ) => {
-    return new Promise<Page>(async (resolve) => {
-      await page.goto("https://discord.com/login", {
-        waitUntil: "networkidle2",
-      });
-      const isLoginPage = page.url().includes("login");
-      if (!isLoginPage) {
-        resolve(page);
-      }
-      await page.waitForSelector('input[name="email"]');
-      await page.type('input[name="email"]', credentials.email);
-      await page.type('input[name="password"]', credentials.password);
-
-      await Promise.all([
-        page.waitForNavigation({ waitUntil: "networkidle2" }),
-        page.click('button[type="submit"]'),
-      ]);
-
-      resolve(page);
-    });
-  };
-
-  const browser = await puppeteer.launch({
-    // headless: false,
-    slowMo: 50,
-  });
-
-  const accounts = [
-    {
-      email: globalThis.SECRETS.DISCORD_EMAIL_1,
-      password: globalThis.SECRETS.DISCORD_PASS_1,
-    },
-    {
-      email: globalThis.SECRETS.DISCORD_EMAIL_2,
-      password: globalThis.SECRETS.DISCORD_PASS_2,
-    },
-  ].filter((acc) => acc.email && acc.password);
-
-  const topics = [
-    globalThis.SECRETS.NTFY_TOPIC_BRAVE as string,
-    globalThis.SECRETS.NTFY_TOPIC_EDGE as string,
-  ];
-
-  let topicIndex = 0;
-
-  const results = await Promise.allSettled(
-    accounts.map(async (acc) => {
-      const page = await browser.newPage();
-      return () => login(page, acc);
-    })
-  );
-
-  results.map((result) => {
-    if (result.status === "fulfilled") {
-      const init = result.value;
-      init()
-        .then((page) => visitURL(page, topics[topicIndex++ % topics.length]))
-        .catch((err) => {
-          sendAlert({
-            title: `Unable to load the page`,
-            message: err?.message,
-            icon: ICON_URL.SERVER_ERROR,
-          });
-        });
-    }
-    if (result.status === "rejected") {
-      sendAlert({
-        title: "Unable to load page",
-        icon: ICON_URL.SERVER_ERROR,
-      });
-    }
-  });
-})();
+app.listen(PORT, () => {
+  console.log("listening  on PORT 4000");
+});
